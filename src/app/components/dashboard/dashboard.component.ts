8c65de556fb493b05f0f883496a24fe6bd8e7a79
Josue <230166@utxicotepec.edu.mx>
2025-03-31 14:34:17 -0600

    Add new sidebarComponent

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
-//import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainComponent } from './main/main.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NavbarComponent /*,SidebarComponent*/, CommonModule, MainComponent],
+  imports: [NavbarComponent ,SidebarComponent, CommonModule, MainComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
